9be0f0fc4c66b8799d4704dc52ab09ed056e2f33
Konstantin Stavrinov <kstavrof@gmail.com>
2022-05-11 16:53:41 +0300

    Version 1: FireBase not work

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
